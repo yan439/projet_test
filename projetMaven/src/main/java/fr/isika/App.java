590a2228dbca825e7c8f6ff41d1ea26b3d18c0a8
yan439 <yannicksourigues@yahoo.fr>
2022-03-28 16:53:09 +0200

    premier commit App.java

---
+++ projetMaven/src/main/java/fr/isika/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.isika;
+
+/**
+ * @author Yannick
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
